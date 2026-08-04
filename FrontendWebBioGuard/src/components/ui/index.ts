179c4aeaf5a59e89d25492cd71103499990ea00c
marisolv985 <23301249@uttt.edu.mx>
2026-08-03 22:48:20 -0600

    clean: eliminar todo el contenido

--- FrontendWebBioGuard/src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { Card } from './Card';
-export { Modal } from './Modal';
-export { LoadingSpinner, LoadingOverlay, EmptyState } from './Feedback';
-export { MetricCard } from './MetricCard';
-export { Badge } from './Badge';
-export { Logo } from './Logo';
--- FrontendWebBioGuard/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export { authService } from './authService';
-export { planService, pagoService } from './planService';
-export { pacienteService } from './pacienteService';
-export { sensorService, reporteService, mlService } from './sensorService';
-export { usuarioService } from './usuarioService';
-export { cuidadorService } from './cuidadorService';
